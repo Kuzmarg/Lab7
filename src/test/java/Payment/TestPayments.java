ddec62082c802671541a5bcea14403057f9c7157
Kuzmarg <vovakuz45616@gmail.com>
2022-11-10 09:57:49 +0200

    Update TestPayments.java

--- src/test/java/Payment/TestPayments.java
+++ src/test/java/Payment/TestPayments.java
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Test;
 public class TestPayments {
     @Test
     public void testCard() {
-        Payment card = new CreditCardPayment();
-        Assertions.assertEquals(card.pay(40), "Credit card payment is successful");
+        Payment card = new CreditCardPayment(40);
+        Assertions.assertEquals(card.pay(), "Credit card payment is successful");
     }
     @Test
     public void testPayPal() {
-        Payment paypal = new PayPalPayment();
-        Assertions.assertEquals(paypal.pay(40), "PayPal payment is successful");
+        Payment paypal = new PayPalPayment(40);
+        Assertions.assertEquals(paypal.pay(), "PayPal payment is successful");
     }
 }
